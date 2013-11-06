e73d9e42d59959b4d1dd573c61a239b9952174df
anton <tohach@gmail.com>
2013-11-06 15:30:47 +0400

    fixed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  subject { page }
+  subject {page}
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 	
 	it "should have the right links on the layout" do
