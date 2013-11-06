4cab86e146180e462713629dd14993dd3d52a036
tohach <tohach@gmail.com>
2013-11-06 17:48:50 +0400

    another try

--- config/application.rb
+++ config/application.rb
@@ -67,9 +67,10 @@ module SampleApp
     # Enable the asset pipeline
     config.assets.enabled = true
     
-
+		#pre-compliing issue		
+	config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
