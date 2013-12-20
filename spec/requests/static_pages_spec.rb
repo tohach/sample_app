68ac579158db7d5a4cc17ee65720c5350eff1091
tohach <tohach@gmail.com>
2013-12-20 23:18:56 +0400

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,7 +35,9 @@ describe "Static pages" do
 	 
 	it "should have the right links on the layout" do
 	  visit root_path
-	  click_link "About"
+	  click_link "Sign in"
+    page.should have_selector 'title', text: full_title('Sign in')
+    click_link "About"
 	  page.should have_selector 'title', text: full_title('About Us')
 	  click_link "Help"
 	  page.should have_selector 'title', text: full_title('Help')
@@ -45,6 +47,7 @@ describe "Static pages" do
 	  click_link "Sign up now!"
 	  #click_link "sample"
 	  page.should have_selector 'title', text: full_title('Sign up')
+
 	
 	end 
 
