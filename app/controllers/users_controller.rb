85fbfc2a287fd2b102652444d2f4747319093023
tohach <tohach@gmail.com>
2013-12-24 14:45:17 +0400

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user,   only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
     
@@ -19,6 +27,51 @@ class UsersController < ApplicationController
     end    
   end
   
-  
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #handle a succesful update.
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    User.find(params[:id]).destroy
+    flash[:success] = "User #{user.id} with name: #{user.name} and email: #{user.email} is destoryed"
+    user.delete
+    redirect_to users_path
+
+  end
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    name "Anton Fedorov"
-    email "tohach@gmail.com"
+    sequence(:name) { |n| "Person #{n}"}
+    sequence(:email) { |n| "Person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
-    
+
+    factory :admin do
+      admin true
+    end
   end
 end
 
