58b3ab0dc0cad0f741e53e2d0b443d13f39eefb1
tohach <tohach@gmail.com>
2013-12-28 11:36:37 +0400

    Add some missting tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,14 @@ describe User do
         end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end
   end
-  
+
+  describe "with admin attributes se to 'true '" do
+    before {@user.toggle!(:admin)}
+
+    it {should be_admin}
+  end
+
+
   describe "when name is not present" do
     before {@user.name = " "}
     it {should_not be_valid}
